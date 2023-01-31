098f12dba17ce082b51b8a5a4855d24e53d74827
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-02-01 00:57:54 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const {Telegram : {WebApp}} = window
 
-export default App;
+export const App = () => {
+    const onClose = () => {
+        WebApp.close();
+    }
+
+    useEffect(() => {
+        WebApp.ready();
+    },[])
+
+    return (
+        <div>
+            <button
+                onClick={onClose}
+            >
+                Close
+            </button>
+        </div>
+    );
+};
