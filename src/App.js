893f865cb58f26eb4cae8b017b4921e898266c33
Anton <108577345+makushenkoao@users.noreply.github.com>
2023-02-01 01:20:11 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import {Route, Routes} from "react-router-dom";
-import {Form, ProductList} from './components'
-
+import {Form, Header, ProductList} from './components'
 const {Telegram : {WebApp}} = window
 
 export const App = () => {
@@ -14,17 +13,12 @@ export const App = () => {
     },[])
 
     return (
-        <div>
-            <button
-                onClick={onClose}
-            >
-                Close
-            </button>
-            <p>Makushenko</p>
+        <>
+            <Header/>
             <Routes>
                 <Route index element={<ProductList/>}/>
                 <Route path={'/form'} element={<Form/>}/>
             </Routes>
-        </div>
+        </>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import {AppBar, Toolbar, Typography, Button, Tooltip} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 export const Header = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <AppBar position='static' color='transparent' sx={{mb : 2}}>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Typography
+                        variant='h5' sx={{fontWeight: 'bold'}}
+                    >
+                        username
+                    </Typography>
+                    <Tooltip title="Close page">
+                        <Button
+                            sx={
+                                {
+                                    bgcolor: 'var(--tg-theme-button-color)',
+                                    color: 'var(--tg-theme-button-text-color)'
+                                }}
+                            variant="contained"
+                        >
+                            <CloseIcon/>
+                        </Button>
+                    </Tooltip>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 };
+
